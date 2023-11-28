29af9e431b40aa625234150cf142fe11d0635bc0
Samcoodess <sdeo@caldwell.edu>
2023-11-27 23:56:08 -0600

    Allowed hosts updated

--- puddle/settings.py
+++ puddle/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-j4ippt+3h39u4ontllpc8a(4h&^god(7aicz#@q^sl_(w)2otp
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.campusmarket.tech', '*']
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
